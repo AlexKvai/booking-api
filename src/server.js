a3f43d4136e875bc6dcb8c93d2adb342f7eb572a
Alexey Parfenov <alexp-2004@mail.ru>
2025-11-09 19:00:28 +0700

    Добавил миграцию в server перед запуском

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
 import dotenv from 'dotenv'
 import app from './app.js'
+import { migrate } from './migrate.js'
+
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-	console.log(`Server running on http://localhost:${PORT}`)
-})
+
+const startServer = async () => {
+	try {
+		await migrate()
+		app.listen(PORT, () => {
+			console.log(`Server running on http://localhost:${PORT}`)
+		})
+	} catch (err) {
+		console.error('Ошибка миграции:', err)
+		process.exit(1)
+	}
+}
+
+startServer()
